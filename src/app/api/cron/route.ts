645ab08c292d04696e6c3ce139accf1a488edfc9
John Stringer <John Stringer>
2024-07-28 16:38:36 +0100

    add dato backup

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,11 +1,28 @@
-export async function GET() {
-  const result = await fetch(
-    "http://worldtimeapi.org/api/timezone/America/Chicago",
-    {
-      cache: "no-store",
-    }
-  );
-  const data = await result.json();
+import { buildClient } from "@datocms/cma-client-node";
 
-  return Response.json({ datetime: data.datetime });
+export async function GET() {
+  const client = buildClient({
+    apiToken: process.env.DATOCMS_FULLACCESS_TOKEN as string,
+  });
+
+  const environments = await client.environments.list();
+
+  const mainEnvironment = environments.find(
+    (environment) => environment.meta.primary
+  );
+
+  const previousUnusedWeeklyBackup = environments.find(
+    (environment) =>
+      environment.id.match("backup-plugin-weekly") && !environment.meta.primary
+  );
+
+  if (previousUnusedWeeklyBackup) {
+    await client.environments.destroy(previousUnusedWeeklyBackup.id);
+  }
+
+  await client.environments.fork(mainEnvironment!.id, {
+    id: `backup-plugin-weekly-${new Date().toISOString().split("T")[0]}`,
+  });
+
+  return Response.json({ status: 200 });
 }
